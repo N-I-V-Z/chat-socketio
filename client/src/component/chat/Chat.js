9a55d36947bf3085269e28da08abd6b20c973256
N-I-V-Z <vothanhnhan106@gmail.com>
2024-08-06 11:37:38 +0700

    add comment

--- client/src/component/chat/Chat.js
+++ client/src/component/chat/Chat.js
@@ -8,28 +8,33 @@ import { CiLogout } from "react-icons/ci";
 import { FaVideo } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../store/actions/authAction";
-import { Modal, notification, Button } from "antd";
+import { Modal, notification, Button, message } from "antd";
+const antdMessage = message; // trùng tên biến nên khai báo 1 biến khác để dùng
 
 const socket = io(`${config.API_ROOT}`);
 
 const Chat = () => {
+  // chuyển trang và lưu redux
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  // thông tin người dùng, room
   const [receiverId, setReceiverId] = useState("");
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
   const { userId } = useSelector((state) => state.auth);
   const [userList, setUserList] = useState([]);
+  // liên quan đến tin nhắn
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const messageListEndRef = useRef(null);
-
+  // liên quan đến kết nối video call
   const [peerConnection, setPeerConnection] = useState(null);
   const localVideoRef = useRef(null);
   const remoteVideoRef = useRef(null);
   const [isCallModalVisible, setIsCallModalVisible] = useState(false);
   const [calling, setCalling] = useState(false);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
+  // hàm xử lý khi cuộc gọi thật sự bắt đầu
   const startCall = useCallback(async () => {
+    // tạo 1 peer
     const pc = new RTCPeerConnection({
       iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
     });
@@ -46,7 +51,7 @@ const Chat = () => {
     pc.ontrack = (event) => {
       remoteVideoRef.current.srcObject = event.streams[0];
     };
-
+    // lấy thông tin từ video, audio
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
         video: true,
@@ -60,59 +65,66 @@ const Chat = () => {
           pc.addTrack(track, stream);
         }
       });
-      setPeerConnection(pc);
 
+      // set vào peer connection
+      setPeerConnection(pc);
+      // tạo 1 offer để set vào local description trong peer của mình (mình cần phải set thêm 1 remote description vào trong peer, đó là offer của người mình muốn kết nối để gọi)
       const offer = await pc.createOffer();
       await pc.setLocalDescription(offer);
-
+      // sau khi set vào local xong thì gửi offer đó qua cho người cần kết nối để họ set vào remote description trong peer của họ (dùng real time để gửi)
       socket.emit("offer", { offer, to: receiverId });
     } catch (error) {
-      console.error("Error accessing media devices or starting call:", error);
+      antdMessage.error("Error accessing media devices or starting call"); // báo lỗi khi không có quyền truy cập camera hoặc audio
     }
   }, [receiverId]);
 
+  // useEffect này dùng để đón các thông tin từ socket gửi về
   useEffect(() => {
+    // hàm xử lý sau khi chấp nhận yêu cầu cuộc gọi
     const handleAcceptCall = (from) => {
       setReceiverId(from);
       setIsCallModalVisible(true);
+      // gửi yêu cầu đã được chấp nhận về cho người kia
       socket.emit("callAccepted", { from: userId, to: from });
       startCall();
     };
-
+    // người dùng tự động đăng kí vào một mảng tạm được lưu ở server
     socket.emit("register", userId);
-
+    // nhận tin nhắn
     socket.on("receiveMessage", ({ senderId, message }) => {
       setMessages((prevMessages) => [...prevMessages, { senderId, message }]);
     });
-
+    // nhận yêu cầu call video
     socket.on("videoCallRequest", ({ from }) => {
       Modal.confirm({
         title: `Bạn có muốn nhận cuộc gọi video từ ${from}?`,
         okText: "Nghe",
         cancelText: "Từ chối",
         onOk() {
+          // nếu đồng ý thì gọi hàm handleAcceptCall() để bắt đầu cuộc gọi
           handleAcceptCall(from);
         },
+        // không đồng ý thì gửi thông tin không đồng ý cho người kia
         onCancel() {
           socket.emit("callRejected", { to: from });
         },
       });
     });
-
+    // nhận thông tin yêu cầu call đã được đồng ý
     socket.on("callAccepted", ({ from }) => {
       setReceiverId(from);
       setCalling(false);
       startCall();
       setIsCallModalVisible(true);
     });
-
+    // nhận thông tin yêu cầu call bị từ chối
     socket.on("callRejected", () => {
       notification.info({
         message: "Cuộc gọi video bị từ chối.",
       });
       setCalling(false);
     });
-
+    // nhận thông tin khi cuộc gọi bị kết thúc
     socket.on("callEnded", () => {
       setIsCallModalVisible(false);
       setCalling(false);
@@ -124,24 +136,25 @@ const Chat = () => {
         message: "Cuộc gọi đã kết thúc.",
       });
     });
-
+    // nhận offer của người kia để lưu vào remote description trong peer connection của mình
     socket.on("offer", async ({ offer }) => {
       if (!peerConnection) return;
 
       try {
+        // set offer nhận được vào remote description của mình
         await peerConnection.setRemoteDescription(
           new RTCSessionDescription(offer)
         );
-
+        // sau đó tạo một offer của mình và set vào local description trong peer connection
         const answer = await peerConnection.createAnswer();
         await peerConnection.setLocalDescription(answer);
-
+        // sau đó gửi offer của mình lại cho người kia để họ set vào remote description của họ
         socket.emit("answer", { answer, to: receiverId });
       } catch (error) {
         console.log(error);
       }
     });
-
+    // nhận offer sau khi mình gửi offer cho họ
     socket.on("answer", async ({ answer }) => {
       if (!peerConnection) return;
 
@@ -158,9 +171,8 @@ const Chat = () => {
       if (!peerConnection) return;
 
       try {
-        await peerConnection.addIceCandidate(
-          new RTCIceCandidate(candidate.iceCandidate)
-        );
+        const iceCandidate = candidate.iceCandidate;
+        await peerConnection.addIceCandidate(new RTCIceCandidate(iceCandidate));
       } catch (error) {
         console.error("Error adding ICE candidate:", error);
       }
@@ -179,6 +191,7 @@ const Chat = () => {
   }, [userId, peerConnection, receiverId, startCall]);
 
   useEffect(() => {
+    // lấy list user đã được lưu
     const fetchUserList = async () => {
       try {
         const response = await axios.post(
@@ -194,6 +207,7 @@ const Chat = () => {
   }, [userId]);
 
   useEffect(() => {
+    // lấy lại những tin nhắn cũ trong phòng
     if (receiverId) {
       const fetchMessages = async () => {
         try {
@@ -215,10 +229,12 @@ const Chat = () => {
     }
   }, [receiverId, userId]);
 
+  // cuộn xuống cuối khi có tin nhắn mới
   useEffect(() => {
     messageListEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
+  // xử lý gửi tin nhắn
   const sendMessage = async () => {
     if (message && receiverId) {
       socket.emit("sendMessage", { senderId: userId, receiverId, message });
@@ -246,12 +262,13 @@ const Chat = () => {
     }
   };
 
+  // nhấn enter để gửi tin
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       sendMessage();
     }
   };
-
+  // popup hỏi đăng xuất
   const showConfirm = () => {
     Modal.confirm({
       title: "Bạn có chắc chắn muốn đăng xuất?",
@@ -262,17 +279,17 @@ const Chat = () => {
       },
     });
   };
-
+  // đăng xuất
   const handleLogout = () => {
     dispatch(logout());
     navigate(`/`);
   };
-
+  // gửi yêu cầu call video
   const requestVideoCall = () => {
     socket.emit("videoCallRequest", { from: userId, to: receiverId });
     setCalling(true);
   };
-
+  // hủy call
   const handleCallCancel = () => {
     setIsCallModalVisible(false);
     socket.emit("callEnded", { to: receiverId });
@@ -281,7 +298,7 @@ const Chat = () => {
       setPeerConnection(null);
     }
   };
-
+  // hủy khi đang chờ chấp nhận call
   const handleCallingCancel = () => {
     socket.emit("callEnded", { to: receiverId });
     if (peerConnection) {
@@ -296,6 +313,7 @@ const Chat = () => {
     <div className="container">
       <div className="sidebar">
         <h2>Danh sách</h2>
+        {/* lấy list user */}
         <ul className="user-list">
           {userList.map((user) => (
             <li
@@ -310,8 +328,8 @@ const Chat = () => {
           ))}
         </ul>
       </div>
-
       <div className="chat-area">
+        {/* các nút như logout, call video */}
         <div className="top-bar">
           {receiverId && (
             <div onClick={requestVideoCall} className="video-call-btn">
@@ -324,6 +342,7 @@ const Chat = () => {
             <CiLogout />
             Logout
           </div>
+          {/* nơi hiện đoạn tin và ô input */}
         </div>
         {receiverId ? (
           <>
@@ -357,6 +376,7 @@ const Chat = () => {
           </div>
         )}
       </div>
+      {/* popup cuộc gọi video */}
       <Modal
         title="Cuộc gọi video"
         visible={isCallModalVisible}
@@ -373,6 +393,8 @@ const Chat = () => {
           <video ref={remoteVideoRef} autoPlay></video>
         </div>
       </Modal>
+
+      {/* popup khi đang chờ chấp nhận call video */}
       <Modal
         title="Đang gọi"
         visible={calling}
